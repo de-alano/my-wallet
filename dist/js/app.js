99425322c9af7ebc3f4165eda0e34ebabf4e860e
Alan Dominiczak <alan.dominiczak10@gmail.com>
2019-06-07 13:54:34 +0200

    Implement the module pattern

---
+++ dist/js/app.js
@@ -0,0 +1,16 @@
+// WALLET CONTROLLER
+const walletController = (() => {
+
+})();
+//---------------------------------------------------------------------------------------------------------//
+
+// USER INTERFACE CONTROLLER
+const UIController = (() => {
+
+})();
+//---------------------------------------------------------------------------------------------------------//
+
+// APP CONTROLLER
+const appController = ((walletCtrl, UICtrl) => {
+
+})(walletController, UIController);
