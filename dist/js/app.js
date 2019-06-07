2e3004ff55cf6dddd3cddcad06aa44ce79cf7d96
Alan Dominiczak <alan.dominiczak10@gmail.com>
2019-06-07 14:24:52 +0200

    Get the inputs data

--- dist/js/app.js
+++ dist/js/app.js
@@ -7,15 +7,43 @@ const walletController = (() => {
 // USER INTERFACE CONTROLLER
 const UIController = (() => {
 
+    // HTML elements
+    const DOMelements = {
+        itemType: '.transactions__type',
+        itemDescription: '.transactions__description',
+        itemValue: '.transactions__value',
+        form: '.transactions__form',
+    };
+
+    return {
+        getInput: () => {
+
+            // Return object with inputs value
+            return {
+                type: document.querySelector(DOMelements.itemType).value, // inc or exp
+                description: document.querySelector(DOMelements.itemDescription).value,
+                value: document.querySelector(DOMelements.itemValue).value
+            };
+        },
+
+        getDOMelements: () => {
+
+            return DOMelements;
+        }
+    };
+
 })();
 //---------------------------------------------------------------------------------------------------------//
 
 // APP CONTROLLER
 const appController = ((walletCtrl, UICtrl) => {
 
+    const DOM = UICtrl.getDOMelements();
+
     const addItem = () => {
         // 1. Get the inputs data
-
+        const input = UICtrl.getInput();
+        console.log(input);
         // 2. Add the item to the wallet controller
 
         // 3. Add the item to the user interface controller
@@ -24,11 +52,10 @@ const appController = ((walletCtrl, UICtrl) => {
 
         // 5. Display the state of wallet on the user interface
 
-        console.log('It works!');
     }
 
     // Add event listener on form submit
-    document.querySelector('.transactions__form').addEventListener('submit', e => {
+    document.querySelector(DOM.form).addEventListener('submit', e => {
         e.preventDefault();
         addItem();
     });
