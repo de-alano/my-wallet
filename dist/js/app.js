885388bb247342209537f6f57e2949bbbd0d5951
Alan Dominiczak <alan.dominiczak10@gmail.com>
2019-06-07 14:11:34 +0200

    Setup event listeners

--- dist/js/app.js
+++ dist/js/app.js
@@ -13,4 +13,31 @@ const UIController = (() => {
 // APP CONTROLLER
 const appController = ((walletCtrl, UICtrl) => {
 
+    const addItem = () => {
+        // 1. Get the inputs data
+
+        // 2. Add the item to the wallet controller
+
+        // 3. Add the item to the user interface controller
+
+        // 4. Calculate the state of wallet
+
+        // 5. Display the state of wallet on the user interface
+
+        console.log('It works!');
+    }
+
+    // Add event listener on form submit
+    document.querySelector('.transactions__form').addEventListener('submit', e => {
+        e.preventDefault();
+        addItem();
+    });
+
+    // Add event listener on press enter
+    document.addEventListener('keypress', e => {
+        if (e.keyCode === 13 || e.which === 13) {
+            addItem();
+        }
+    });
+
 })(walletController, UIController);
