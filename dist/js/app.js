ab77d2d7bb21a60b07d17f81007db8510d60a9e3
Alan Dominiczak <alan.dominiczak10@gmail.com>
2019-06-07 15:01:53 +0200

    Add a new item to the wallet controller

--- dist/js/app.js
+++ dist/js/app.js
@@ -28,6 +28,37 @@ const walletController = (() => {
         }
     };
 
+    return {
+        addItem: (type, des, val) => {
+            let newItem, ID;
+
+            // [1 2 3 4 5 6], next ID = 6
+            // [1 2 3 4 6 8], next ID = 9
+            // ID = last ID + 1
+
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') newItem = new Expense(ID, des, val);
+            else if (type === 'inc') newItem = new Income(ID, des, val);
+
+            // Push the new item into data structure
+            data.allItems[type].push(newItem);
+
+            // Return the new item
+            return newItem;
+        },
+
+        test: () => {
+            console.log(data);
+        }
+    };
+
 })();
 //---------------------------------------------------------------------------------------------------------//
 
@@ -65,12 +96,12 @@ const UIController = (() => {
 // APP CONTROLLER
 const appController = ((walletCtrl, UICtrl) => {
 
-    const addItem = () => {
+    const ctrlAddItem = () => {
         // 1. Get the inputs data
         const input = UICtrl.getInput();
         console.log(input);
         // 2. Add the item to the wallet controller
-
+        const newItem = walletCtrl.addItem(input.type, input.description, input.value);
         // 3. Add the item to the user interface controller
 
         // 4. Calculate the state of wallet
@@ -85,13 +116,13 @@ const appController = ((walletCtrl, UICtrl) => {
         // Add event listener on form submit
         document.querySelector(DOM.form).addEventListener('submit', e => {
             e.preventDefault();
-            addItem();
+            ctrlAddItem();
         });
 
         // Add event listener on press enter
         document.addEventListener('keypress', e => {
             if (e.keyCode === 13 || e.which === 13) {
-                addItem();
+                ctrlAddItem();
             }
         });
     };
